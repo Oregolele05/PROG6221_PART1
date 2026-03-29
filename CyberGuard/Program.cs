f4aff1515e8acbb4d70a18b62906c50976acbad9
Onkemetse05 <oregolelemangwegape05@gmail.com>
2026-03-29 19:51:39 +0200

    created relevant code within Userinteraction() which will ask user for their name and also validate input

--- CyberGuard/Program.cs
+++ CyberGuard/Program.cs
@@ -18,6 +18,30 @@ class CyberGuard
     }
     public void UserInteraction()
     {
+        while (true)
+        {
+            //this is for the text colour
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            //ask user for their name
+            Console.Write("What is your name: ");
+            string name = Console.ReadLine().Trim();
+
+            //this code will validate the user input for name
+            if (name.IsWhiteSpace())
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Please enter a valid name.");
+                continue;
+            }
+            else if (name.Any(char.IsDigit))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("A name cannot contain numeric values.");
+                continue;
+            }
+            Console.WriteLine("welcome " + name + " nice to meet you!!\n");
+            break;
+        }
 
     }
     public void ResponseSystem()
