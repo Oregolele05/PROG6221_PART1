16b010ac8e6eec5aac07c4d7c91ac562b8550559
Oregolele <oregolelemangwegape05@gmail.com>
2026-05-12 22:47:58 +0200

    Add files via upload

---
+++ ASSIGNMENT/PART2/CyberGuard/CyberDesign.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Media;
+
+public class CyberDesign
+{
+    //this is for the voice greeting
+    public void VoiceGreeting()
+    {
+        try
+        {
+            SoundPlayer player = new SoundPlayer(@"C:\\Users\\gmkin\\Documents\\second year\\PROG6221\\ASSIGNMENT\\PART1\\CyberGuard\\greet.wav");
+            player.PlaySync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error playing sound: " + ex.Message);
+        }
+    }
+    //this is for the text animation effect
+    public void Animation(string text, int delay = 50)
+    {
+        foreach(char c in text)
+        {
+            Console.Write(c);
+            Thread.Sleep(delay);
+        }
+    }
+    //this is for the logo design
+    public void LogoDisplay()
+    {
+        Console.WriteLine("╔════════════════════════════════════════════════════════════════════════════════════╗\r\n║  ██████╗██╗   ██╗██████╗ ███████╗██████╗  ██████╗ ██╗   ██╗ █████╗ ██████╗ ██████╗ ║\r\n║ ██╔════╝╚██╗ ██╔╝██╔══██╗██╔════╝██╔══██╗██╔════╝ ██║   ██║██╔══██╗██╔══██╗██╔══██╗║\r\n║ ██║      ╚████╔╝ ██████╔╝█████╗  ██████╔╝██║  ███╗██║   ██║███████║██████╔╝██║  ██║║\r\n║ ██║       ╚██╔╝  ██╔══██╗██╔══╝  ██╔══██╗██║   ██║██║   ██║██╔══██║██╔══██╗██║  ██║║\r\n║ ╚██████╗   ██║   ██████╔╝███████╗██║  ██║╚██████╔╝╚██████╔╝██║  ██║██║  ██║██████╔╝║\r\n║  ╚═════╝   ╚═╝   ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ║\r\n╚════════════════════════════════════════════════════════════════════════════════════╝");
+    }
+    public void Box(string text)
+    {
+        int width = text.Length + 2;
+        //this will wrap the users welcome message in a box
+        Console.WriteLine("╔" + new string('═', width) + "╗");
+        Console.WriteLine("║ " + text + " ║");
+        Console.WriteLine("╚" + new string('═', width) + "╝");
+    }
+}
