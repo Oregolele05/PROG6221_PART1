afc532ba1aa26aebde8199899796e4eb01bf6b74
Onkemetse05 <oregolelemangwegape05@gmail.com>
2026-05-21 23:00:10 +0200

    Added CyberUser class to store session memory including username, topic history and visit tracking

--- ASSIGNMENT/PART2/CyberGuard/CyberUser.cs
+++ ASSIGNMENT/PART2/CyberGuard/CyberUser.cs
@@ -1,13 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 
 
-	public class CyberUser
-	{
-		private string username;
-		private string lastTopic;
-		private string favTopic;
+public class CyberUser
+{
+    public string username = string.Empty;
+    public string lastTopic = string.Empty;
+    public string favTopic = string.Empty;
 
-		private string Section { get; private set } = "getname";
-	}
+    public string Section { get; set; } = "getname";
+
+    public List<string> topicsViewed = new List<string>();
+
+    public Dictionary<string, int> topicCount = new Dictionary<string, int>();
+
+    public void TrackTopic(string topic)
+    {
+        if (!topicsViewed.Contains(topic))
+        {
+            topicsViewed.Add(topic);
+        }
+        if (topicCount.ContainsKey(topic))
+        {
+            topicCount[topic]++;
+        }
+        else
+        {
+            topicCount[topic] = 1;
+        }
+        int max = 0;
+        foreach (var count in topicCount)
+        {
+            if(count.Value > max)
+            {
+                max = count.Value;
+                favTopic = count.Key;
+
+            }
+            
+        }
+        lastTopic = topic;
+    }
+
+}
 
 
